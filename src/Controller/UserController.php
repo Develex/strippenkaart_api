94fdd591a495ae68777243de2f53c838d52e8f74
develex <collinfranckena77@gmail.com>
2020-03-10 12:35:34 +0100

    Develex - Created UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class UserController
+ * @package App\Controller
+ * @Route("/user")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+}
