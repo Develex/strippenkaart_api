b95264a8a0005607723513e56ea20fbffc32c834
Develex <collinfranckena77@gmail.com>
2020-11-03 10:57:11 +0100

    removed nelmio-cors

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
