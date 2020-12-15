97002291e6d2e43d7e1c803dc1b3bdad47aff6b1
Develex <collinfranckena77@gmail.com>
2020-12-15 09:33:40 +0100

    fixed migrations

---
+++ src/Repository/StrippenRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Stripcard;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Stripcard|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Stripcard|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Stripcard[]    findAll()
+ * @method Stripcard[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class StrippenRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Stripcard::class);
+    }
+
+    // /**
+    //  * @return Stripcard[] Returns an array of Stripcard objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Stripcard
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
